4688c9eb2d8513f1318eea012637ea7042ab8f58
L-S-2020 <67273491+L-S-2020@users.noreply.github.com>
2023-10-09 10:58:29 +0000

    Update

--- create.py
+++ create.py
@@ -1,9 +1,10 @@
 from huggingface_hub import InferenceClient#
 import newspaper
-import requests, json
+import requests, json, time
 
 client = InferenceClient(
-    "mistralai/Mistral-7B-Instruct-v0.1"
+    "mistralai/Mistral-7B-Instruct-v0.1",
+    "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"
 )
 
 def format_prompt(message, history):
@@ -15,7 +16,7 @@ def format_prompt(message, history):
   return prompt
 
 def generate(
-    prompt, history, temperature=0.9, max_new_tokens=256, top_p=0.95, repetition_penalty=1.0,
+    prompt, history, temperature=0.9, max_new_tokens=5000, top_p=0.95, repetition_penalty=1.0,
 ):
     temperature = float(temperature)
     if temperature < 1e-2:
@@ -35,27 +36,35 @@ def generate(
 
     stream = client.text_generation(formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=False)
     output = ""
-
     for response in stream:
-        output += response.token.text
-        yield output
+        output += response.token['text']
     return output
 
-file = open("websites.txt")
+'''file = open("websites.txt")
 
-#portal = newspaper.build('http://www.faz.net')
-#print(portal)
+portal = newspaper.build('http://www.faz.net')
+print(portal)
 
-#for article in portal.articles:
-#    print(article.url)
-#    article.download()
-#    article.parse()
-#    print(article.summary)
+for article in portal.articles:
+    print(article.url)
+    article.download()
+    article.parse()
+    print(article.summary)
+    time.sleep(2)'''
+
 
 r = requests.get('https://newsdata.io/api/1/news?apikey=pub_2430005b32b1160236e845cf2ded9a93e1eb0&country=de')
 response = json.loads(r.text)
-print(response)
-for i in response.results:
-   article = json.loads(i)
-   print(article.title)
+results = response['results']
+for i in results:
+   title = i['title']
+   url = i['link']
+   description = i['description']
+   content = i['content']
+   id = i['article_id']
 
+   prompt = f"Schreibe den Artikel um, verwende interessante und fesselnde Sprache, erfinde nichts hinzu. /n Artikel: {title} /n {description} /n {content} /n"
+   print(prompt)
+   output = generate(prompt, [])
+   print(output)
+   
