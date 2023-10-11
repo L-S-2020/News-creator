0cbe43bad11cebc2cacb1da0d83f6db93c612237
L-S-2020 <leostewal@gmail.com>
2023-10-11 20:16:19 +0200

    GPT-3.5 hinzugefügt

--- create.py
+++ create.py
@@ -1,57 +1,14 @@
-from huggingface_hub import InferenceClient#
-import newspaper
-import requests, json, time
-
-client = InferenceClient(
-    "mistralai/Mistral-7B-Instruct-v0.1",
-    "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"
-)
-
-def format_prompt(message, history):
-  prompt = "<s>"
-  for user_prompt, bot_response in history:
-    prompt += f"[INST] {user_prompt} [/INST]"
-    prompt += f" {bot_response}</s> "
-  prompt += f"[INST] {message} [/INST]"
-  return prompt
-
-def generate(
-    prompt, history, temperature=0.9, max_new_tokens=5000, top_p=0.95, repetition_penalty=1.0,
-):
-    temperature = float(temperature)
-    if temperature < 1e-2:
-        temperature = 1e-2
-    top_p = float(top_p)
-
-    generate_kwargs = dict(
-        temperature=temperature,
-        max_new_tokens=max_new_tokens,
-        top_p=top_p,
-        repetition_penalty=repetition_penalty,
-        do_sample=True,
-        seed=42,
+import requests, json, time, g4f
+from g4f.Provider import *
+def generate(prompt):
+    g4f.logging = True  # enable logging
+    g4f.check_version = False  # Disable automatic version checking
+    antwort = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+        provider=g4f.Provider.AItianhu,
     )
-
-    formatted_prompt = format_prompt(prompt, history)
-
-    stream = client.text_generation(formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=False)
-    output = ""
-    for response in stream:
-        output += response.token['text']
-    return output
-
-'''file = open("websites.txt")
-
-portal = newspaper.build('http://www.faz.net')
-print(portal)
-
-for article in portal.articles:
-    print(article.url)
-    article.download()
-    article.parse()
-    print(article.summary)
-    time.sleep(2)'''
-
+    return antwort
 
 r = requests.get('https://newsdata.io/api/1/news?apikey=pub_2430005b32b1160236e845cf2ded9a93e1eb0&country=de')
 response = json.loads(r.text)
@@ -61,10 +18,27 @@ for i in results:
    url = i['link']
    description = i['description']
    content = i['content']
+   content = content.lstrip(".")
    id = i['article_id']
 
-   prompt = f"Schreibe den Artikel um, verwende interessante und fesselnde Sprache, erfinde nichts hinzu. /n Artikel: {title} /n {description} /n {content} /n"
+   prompt = f'''Schreibe den folgenden Artikel um, erfinde nichts hinzu, benutze Fesselnde und informative Sprache.
+Benutze folgendes Layout:
+Titel:
+Beschreibung:
+Inhalt:
+Keywords:
+Kategorie:
+
+Artikel:
+Titel: {title}
+Beschreibung: {description}
+Inhalt: {content}
+'''
    print(prompt)
-   output = generate(prompt, [])
+   print(""
+         "")
+   output = generate(prompt)
    print(output)
+   time.sleep(2)
+   print("---------------------------------------------------")
    
